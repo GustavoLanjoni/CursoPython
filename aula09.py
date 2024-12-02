1624c6c2bcc28beb2f46c10d66250159d48b5f31
Gustavo Lanjoni <gustavosilva94514@gmail.com>
2024-12-02 01:22:35 -0300

    Estudando

---
+++ aula09.py
@@ -0,0 +1,22 @@
+
+adicao = 10 + 10
+print('adição', adicao)
+
+subtracao = 10 - 5
+print('subtração', subtracao)
+
+multiplicacao = 20 * 9
+print('multiplicação', multiplicacao)
+
+divisao = 5 / 22.4
+print('divisão', divisao)
+
+divisao_inteira = 11 // 40
+print('Divisão Inteira', divisao_inteira)
+
+exponenciacao  = 22 ** 12
+print('Exponecição', exponenciacao)
+
+modulo = 50 % 90
+print('Modulo', modulo)
+
---
+++ aula10.py
@@ -0,0 +1,5 @@
+concatenacao = 'G' + 'U' + 'S' + 'T' + 'A' + 'V' + 'O' + ' ' + 'LANJONI'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+tres_vezes_gustavo = 4 * 'Gustavo'
